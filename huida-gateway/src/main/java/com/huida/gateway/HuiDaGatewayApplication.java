8af8cb9fe792d010b69afa75bbc973e84cc85ebb
dazhengjiang <31353107@qq.com>
2024-12-27 18:15:08 +0800

    初始化

---
+++ huida-gateway/src/main/java/com/huida/gateway/HuiDaGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.huida.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuiDaGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HuiDaGatewayApplication.class, args);
+    }
+}
